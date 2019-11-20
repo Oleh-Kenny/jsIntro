228354a47f2b6d7b5bc62a02346c12be3f044326
Oleh <lemeholeh@gmail.com>
2019-11-21 00:01:24 +0200

    fix cod,and learning to write code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-var app = document.querySelector('.app');
-var api = 'http://omdbapi.com/?apikey=c87c3a96&t=Game';
+var input = document.querySelector('input');
+var text = input.value
+var button = document.querySelector('button');
+button.addEventListener('click',myFilm);
 
+function myFilm(){
+    text = input.value
+    var link = `http://www.omdbapi.com/?apikey=c87c3a96&t=${text}`;
+    var newRec = new Request(link);
+    fetch(newRec)
+    .then((response) =>{
+        return response.json();
+    }) 
+    .then((posty) => {
+        console.log(link)
+        
+        document.querySelector('h1').innerHTML = posty.Title;
+        var post = document.querySelector('.poster')
+        post.innerHTML = `<img src='${posty.Poster}' alt="">`
+       
+    })
+}
